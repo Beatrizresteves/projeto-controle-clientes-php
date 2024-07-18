c89a8623cbda1768d236245c945fb7248fce9d51
Beatrizresteves <beatrizramalho.esteves@gmail.com>
2024-07-17 21:33:12 -0300

    Add Clientecontroler

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cliente; 
+
+class ClienteController extends Controller
+{
+    public function store(Request $request)
+    {
+        // Implementação do método store
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Implementação do método update
+    }
+
+    public function destroy($id)
+    {
+        // Implementação do método destroy
+    }
+
+    public function show($id)
+    {
+        // Implementação do método show
+    }
+
+    public function consultaPorFinalPlaca($numero)
+    {
+        // Implementação do método consultaPorFinalPlaca
+    }
+}
